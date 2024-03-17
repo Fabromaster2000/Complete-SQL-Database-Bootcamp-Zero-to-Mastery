b8d52e36165caf1ded37b08e92ada645c45da340
Fabromaster2000 <86709101+Fabromaster2000@users.noreply.github.com>
2024-03-17 13:39:44 -0300

    Update exercises.sql

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -8,11 +8,13 @@
 
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
+  SELECT AVG(salary) FROM "public"."salaries":
+    63810.744836143706
 
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
-
+    1965-02-01
 
 -- BEFORE YOU START
 /*
@@ -23,6 +25,8 @@
 
 -- Question 1: How many towns are there in france?
 -- Table: Towns
+    SELECT COUNT(id) FROM "public"."towns";
+    36684
 
 
 -- BEFORE YOU START
@@ -35,10 +39,23 @@
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
 
+     SELECT COUNT("language") FROM "public"."countrylanguage"
+WHERE isofficial = true;
+    238
+
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
 
+    66.486036
+
 -- Question 3: What is the average population for cities in the netherlands?
 -- Table: city
+
+    SELECT AVG(population)
+    FROM "public"."city"
+    WHERE countrycode = 'NLD';
+
+
+      AVG = 185001.75
 
 
